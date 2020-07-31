ca726d9a90ec6da596360815d34f7e1ab977b208
ruslan <admin94.tm@mail.ru>
2020-07-31 19:00:04 +0300

    hello first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// созд массивб поиск по массиву клик по дропдовн
+//Data getData
+
+
+
+let formSearch = document.querySelector('.form-search'),
+inputCitiesFrom = document.querySelector('.input__cities-from'),
+dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+inputCitiesTo = document.querySelector('.input__cities-to'),
+dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
+inputDateDepart = document.querySelector('.input__date-depart'),
+cipestTicket = document.getElementById('cheapest-ticket'),
+otherCheapTickets=document.getElementById('other-cheap-tickets'),
+cheapestTicket=document.querySelector('#cheapest-ticket'),
+CheapTickets=document.getElementById('other-cheap-tickets');
+
+
+// let city = ['Москва', 'Санкт-Петербург', 'Керч', 'Махачкала', 'Смоленск', 'Туркменабад', 'Чарджов', 'Мухасранск', 'Стамбул', 'Константинополь', 'Лондон'];
+// let city=[]
+
+// const API_KEY='fb844ccadfe1284ee8204a02b66fa38b';
+// const citesApi='http://api.travelpayouts.com/data/ru/cities.json';
+// const proxy ='https://cors-anywhere.herokuapp.com/';
+// const calendar='http://min-prices.aviasales.ru/calendar_preload';
+// const MAXCOUNT = 10;
+// get data 
+//firlter
+
